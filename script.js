727d1a14b2ea1979cff2b3772f5c1134c39ace63
User <user@Users-MacBook-Air.local>
2023-10-13 23:48:31 -0400

    added addNumeric() function to add a single numeric value to password if numeric characters options is clicked

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+// Criteria Variables
+const pwLength = document.getElementById("password-length");
+const hasSpecialCharacters = document.getElementById("password-special");
+const hasUppercase = document.getElementById("password-uppercase");
+const hasLowercase = document.getElementById("password-lowercase");
+const hasNumeric = document.getElementById("password-numbers");
+
 const characterArray = [
   "a",
   "b",
@@ -29,24 +36,55 @@ const characterArray = [
   "y",
   "z",
 ];
+const numericArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+console.log(hasUppercase.checked);
 
 //  function to generate password
-function generatePassword() {
+function generatePassword(num) {
   const pw = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < num; i++) {
     pw.push(characterArray[Math.floor(characterArray.length * Math.random())]);
   }
   return pw.join("");
 }
 
+function addNumeric(word) {
+  word = word.split("");
+  word[Math.floor(Math.random() * word.length)] =
+    numericArray[Math.floor(Math.random() * numericArray.length)];
 
+  return word.join("");
+}
+
+addNumeric(generatePassword(10));
 
 console.log(generatePassword());
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let password = generatePassword(pwLength.value);
   let passwordText = document.querySelector("#password");
+
+  if (!pwLength.value) {
+    alert("Please enter a value");
+    return;
+  }
+
+  if (pwLength.value > 128) {
+    alert("Password cannot exceed 128 characters");
+    return;
+  }
+
+  if (pwLength.value < 8) {
+    alert("Password cannot be less than 8 characters");
+    return;
+  }
+
+  if (hasNumeric.checked) {
+    password = addNumeric(password);
+  }
 
   passwordText.value = password;
 }
